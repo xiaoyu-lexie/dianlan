3c8e32183265d1e922ed100298b770844b9bc7cc
Xiaoyu Chen <xiaoyuchen.uncc@gmail.com>
2023-04-19 11:56:56 -0700

    bug1

--- pages/products.js
+++ pages/products.js
@@ -1,20 +1,36 @@
 import { Carousel } from "antd";
 import image1 from "../public/images/carousel1.png";
 
+import Image from "next/image";
+
 console.log("image1", image1);
 
 const contentStyle = {
   height: "160px",
-  color: "#fff",
+  color: "red",
   lineHeight: "160px",
   textAlign: "center",
-  background: "#364d79",
+  // background: "#364d79",
+  borderStyle: "solid",
+  borderColor: "red",
+  display: "flex",
 };
 
 const Products = () => (
-  <Carousel autoplay>
+  <Carousel>
     <div>
-      <h3 style={contentStyle}>1</h3>
+      <h3 style={contentStyle}>
+        <div style={{ width: "100%", height: "80%", zIndex: "-1" }}>
+          <Image
+            src={image1}
+            alt="image1"
+            width={"500"}
+            height={"500"}
+            style={{ zIndex: "-1" }}
+          />
+        </div>
+        <div>sdfdskjk</div>
+      </h3>
     </div>
     <div>
       <h3 style={contentStyle}>2</h3>
